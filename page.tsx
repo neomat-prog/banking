95b7273a6c395185b7ee6920b75d7922676a6ca6
Kacper <68641248+neomat-prog@users.noreply.github.com>
2024-07-06 12:50:21 +0200

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,31 @@
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+
+
+import React from "react";
+
+const Home = () => {
+  const loggedIn = { firstName: "Dave" };
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions efficiently"
+          />
+
+          <TotalBalanceBox 
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={8025.29}
+          />
+        </header>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
